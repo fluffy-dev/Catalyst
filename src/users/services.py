8688c08dd048f9bd23d264064194b48a796efbe4
Rauan <asetovrauan95@gmail.com>
2025-07-02 12:34:15 +0500

    service for user model

---
+++ src/users/services.py
@@ -0,0 +1,25 @@
+from django.db import transaction
+
+from src.users.models import User
+
+
+@transaction.atomic
+def user_create(*, email: str, password: str, **extra_fields) -> User:
+    """
+    Creates and saves a new user.
+
+    Args:
+        email: The email address of the user.
+        password: The user's password.
+        extra_fields: Additional fields for the user model.
+
+    Returns:
+        The created User instance.
+    """
+    user = User(email=email, username=email, **extra_fields)
+    user.set_password(password)
+
+    user.full_clean()
+    user.save()
+
+    return user
