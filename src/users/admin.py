3d2bdd643a732b9cee7d4989374c9fa922da820e
Rauan <asetovrauan95@gmail.com>
2025-07-02 12:32:38 +0500

    registered user model to admin panel

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from src.users.models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    """
+    Admin configuration for the custom User model.
+    """
+    pass
